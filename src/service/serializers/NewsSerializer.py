fc7b10184695f649ad76eb0e679a6e6e5665582c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 20:14:22 +0330

    refactor: modify fields

--- src/service/serializers/NewsSerializer.py
+++ src/service/serializers/NewsSerializer.py
@@ -3,10 +3,13 @@ from rest_framework import serializers
 from data import models
 
 class NewsSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only = True)
     tags = serializers.StringRelatedField(many = True)
+
     class Meta:
         model = models.News
         fields = [
+            "id",
             "title",
             "text",
             "source",
