cf749596cc5aed7421591e54609872eae638582e
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 08:28:44 +0330

    chore: api urls

--- src/tech_news/urls.py
+++ src/tech_news/urls.py
@@ -17,7 +17,26 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rest_framework import permissions
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+      title="CoursePlatform API",
+      default_version='v1',
+      description="",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", include("api.urls"))
 ]
