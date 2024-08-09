c73e9fbb62c3a5e1d21c4076bf80c679613f95d4
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 08:28:14 +0330

    chore: add installed apps

--- src/tech_news/settings.py
+++ src/tech_news/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_yasg",
+    "data",
+    "service",
+    "api",
 ]
 
 MIDDLEWARE = [
