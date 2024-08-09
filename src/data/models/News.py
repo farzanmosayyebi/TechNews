09155ff791fa13303ad8425cf5889050423c3cc2
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 10:28:46 +0330

    refactor: modify plural verbose name

--- src/data/models/News.py
+++ src/data/models/News.py
@@ -3,8 +3,11 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length = 255)
     text = models.TextField()
-    source = models.URLField
+    source = models.URLField(null = True, blank = True)
     tags = models.ManyToManyField("Tag")
+
+    class Meta:
+        verbose_name_plural = "News"
 
     def __str__(self) -> str:
         return self.title
