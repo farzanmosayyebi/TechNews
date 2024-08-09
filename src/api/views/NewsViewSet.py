0777645114a6d5962b273e4011945e889accb354
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 12:36:32 +0330

    refactor: general refactoring on filter

--- src/api/views/NewsViewSet.py
+++ src/api/views/NewsViewSet.py
@@ -1,12 +1,12 @@
-from rest_framework import viewsets
-from rest_framework import mixins
-from rest_framework.response import Response
+from django_filters import rest_framework as filters
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import mixins, viewsets
+from rest_framework.response import Response
 
 from data import models
-from service import serializers
-from service import pagination
+from service import filters as appfilters
+from service import pagination, serializers
 
 tag_param = openapi.Parameter("tags",
                             openapi.IN_QUERY,
@@ -19,16 +19,16 @@ class NewsViewSet(mixins.ListModelMixin,
     queryset = models.News.objects.prefetch_related("tags").all()
     serializer_class = serializers.NewsSerializer
     pagination_class = pagination.CustomPagiation
+    filter_backends = [
+        filters.DjangoFilterBackend,
+    ]
+    filterset_class = appfilters.NewsFilter
 
     @swagger_auto_schema(manual_parameters = [tag_param])
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        tags = request.GET.get("tags")
-        if tags is not None:
-            tags = tags.strip().split(",")
-            tag_list = models.Tag.objects.filter(title__in = tags)
-            queryset = queryset.filter(tags__in = tag_list).distinct()
+        queryset = self.get_queryset()
+        filter = appfilters.NewsFilter(data = request.GET, queryset = queryset)
+        queryset = filter.qs
 
         page = self.paginate_queryset(queryset)
         if page is not None:
