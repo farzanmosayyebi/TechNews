5acf1f8fcd21f2abcc32138324a433712f3c40af
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-09 10:28:57 +0330

    chore: api urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,13 @@
 from django import urls
 from api import views
+from rest_framework import routers
 
 urlpatterns = [
-    urls.path("news/", views.NewsListAPIView.as_view())
+    # urls.path("news/", views.NewsListAPIView)
 ]
+
+router = routers.DefaultRouter()
+
+router.register("news", views.NewsViewSet, basename = "news")
+
+urlpatterns += router.urls
