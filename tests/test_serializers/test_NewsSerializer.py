5b109eb2abe4db781e1ba68ed4ad0a89bfc7b727
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-10 08:16:11 +0330

    test: add unit test for News serializer

---
+++ tests/test_serializers/test_NewsSerializer.py
@@ -0,0 +1,30 @@
+import lorem
+from django.test import TestCase
+
+from service import serializers
+from data import models
+
+class NewsSerializerTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        tag1 = models.Tag.objects.create(title = "tag1")
+        tag2 = models.Tag.objects.create(title = "tag2")
+        tag3 = models.Tag.objects.create(title = "tag3")
+
+        news = models.News.objects.create(
+            title = lorem.sentence(),
+            text = lorem.text(),
+            source = "www.somerandomsource.com",
+        )
+        news.tags.add(tag1, tag2, tag3)
+
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_tags_are_retrieved_properly(self):
+        news = models.News.objects.prefetch_related("tags").get(pk = 1)
+        serializer = serializers.NewsSerializer(news)
+        data = serializer.data.get("tags")
+        assert "tag1" in data
+        assert "tag2" in data 
+        assert "tag3" in data
