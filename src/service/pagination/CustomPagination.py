02330e40595d680d512e9b4ac0b661aff1e1ebea
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-13 13:51:25 +0330

    refactor: modify page size

--- src/service/pagination/CustomPagination.py
+++ src/service/pagination/CustomPagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class CustomPagiation(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_query_param = "page"
-    max_page_size = 10
+    max_page_size = 20
