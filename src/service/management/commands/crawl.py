cc7b9eee6f157e29920510e0639701bcdbb9fc64
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-15 10:22:11 +0330

    feat: add argument for limit the crawl process.

--- src/service/management/commands/crawl.py
+++ src/service/management/commands/crawl.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 
 from service.crawlers import ZoomitCrawler
 
@@ -6,8 +6,15 @@ from service.crawlers import ZoomitCrawler
 class Command(BaseCommand):
     help = "Release the spiders"
 
+    def add_arguments(self, parser: CommandParser) -> None:
+        super().add_arguments(parser)
+        parser.add_argument("--limit", 
+                            type = int, 
+                            help = "Maximum number of items to scrape.")
+
     def handle(self, *args, **options):
+        limit = options.get("limit")
         crawler = ZoomitCrawler()
-        item_count = crawler.crawl()
+        item_count = crawler.crawl(limit)
         self.stdout.write(f"Crawled {item_count} items.")
     
