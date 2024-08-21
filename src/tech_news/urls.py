ca1cdda9290c911d14f2379401bdc5d5d40e3700
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-21 14:45:58 +0330

    fix: bug fixes

--- src/tech_news/urls.py
+++ src/tech_news/urls.py
@@ -25,7 +25,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-      title="CoursePlatform API",
+      title="Tech News API",
       default_version='v1',
       description="",
       terms_of_service="https://www.google.com/policies/terms/",
