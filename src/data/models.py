ca4515ad3a1e4f4baa6677e7ba026a2a998462d6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-08-21 14:45:42 +0330

    fix: bug fixes

--- src/data/models.py
+++ src/data/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255, null = True)
     text = models.TextField()
     source = models.URLField(null = True, blank = True)
     tags = models.ManyToManyField("Tag")
